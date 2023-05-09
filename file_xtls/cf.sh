44c1f2d918547312f566293b893e678b21068dcc
bayong00 <67575655+bayong00@users.noreply.github.com>
2023-05-10 00:57:19 +0700

    Update cf.sh

--- file_xtls/cf.sh
+++ file_xtls/cf.sh
@@ -1,7 +1,7 @@
 mkdir -p /etc/xray
 touch /etc/xray/domain
 touch /etc/xray/scdomain
-DOMEN=warungrio.cloud
+domen=warungrio.cloud
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c2)
 domain=client-${sub}.${domen}
 echo "${domain}" > /etc/xray/scdomain
